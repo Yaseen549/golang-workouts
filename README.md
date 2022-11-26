d87be4adcc0413441c2459b26749ce39ddf15869
Yaseen <fantasticyaseenshariff@gmail.com>
2022-11-26 19:54:48 +0530

    day2: hello world explanation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 <h1 align=center>Golang learnings</h1>
 1. p1: hello world!
+2. p2: hello world! explanation
---
+++ p2/hello.go
@@ -0,0 +1,7 @@
+package main // Should be imported and used the same name
+
+import "fmt" // fmt -> short form of format! importing from packages
+
+func main() { // main method of a program | func -> is a function
+	fmt.Println("Hi There!") // fmt.Println -> 'p' must be capital
+}
